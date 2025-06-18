88a11d767bd244bcb0bb3a2317c7f10bd3e24770
eltondeantech <ifechukwunewworld@gmail.com>
2025-06-18 10:37:51 +0100

    commit

---
+++ js file/main.js
@@ -0,0 +1,13 @@
+var Typed = new Typed(".typing-text", {
+  strings: [
+    "website creator",
+    "video editor",
+    "programmer",
+    "web designer(ui/ux)",
+    "ethnical hacker",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
